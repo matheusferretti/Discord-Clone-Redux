fb3dcad60bdb5317bedabe3ed1551ca73175a418
matheusferretti <matheusferretti96@gmail.com>
2021-01-05 15:48:43 -0500

    updating discord

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="app">
       <h1>Let's build a discord clone!!!</h1>
+
+      {/* Sidebar */}
+      <Sidebar />
+      
+      {/* Chat */}
     </div>
   );
 }
