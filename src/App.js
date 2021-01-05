1177fa91cafa97e628f6a7079d39224cb83a14ff
matheusferretti <matheusferretti96@gmail.com>
2021-01-05 16:08:03 -0500

    updating discord

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';]
-import Sidebar from "./Sidebar"
+import './App.css';
+import Sidebar from './Sidebar';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a discord clone!!!</h1>
-
       {/* Sidebar */}
       <Sidebar />
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./Sidebar.css";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <h2>I am the Sidebar</h2>
+
+            <div className="sidebar__top">
+                <h3>SYMP-WEB</h3>
+            </div>
         </div>
     )
 }
