729ab3d3cf01aa84907ba012bd9b0ebfca908fbb
matheusferretti <matheusferretti96@gmail.com>
2021-01-05 16:29:42 -0500

    updating discord

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Sidebar.css";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 function Sidebar() {
     return (
@@ -12,7 +13,16 @@ function Sidebar() {
                 <ExpandMoreIcon />
             </div>
 
-            
+            <div className="sidebar__channels">
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                        <ExpandMoreIcon />
+                        <h4>Text Channels</h4>
+                    </div>
+
+                    <AddIcon className="sidebar__addChannel" />
+                </div>
+            </div>
         </div>
     )
 }
