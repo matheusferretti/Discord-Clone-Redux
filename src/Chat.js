2177a7da435a7d41fa5d8df047edc348e63b5aa1
matheusferretti <matheusferretti96@gmail.com>
2021-01-06 17:01:53 -0500

    updating discord

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import './Chat.css'
 import ChatHeader from './ChatHeader'
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import GifIcon from '@material-ui/icons/Gif';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 
 function Chat() {
     return (
         <div className="chat">
             <ChatHeader />
+
+            <div className="chat__messages">
+
+            </div>
+
+            <div className="chat__input">
+                <AddCircleIcon fontSize="large" />
+                <form>
+                    <input placeholder={`Message #TESTCHANNEL`} />
+                    <button className="chat__inputButton" type="submit">
+                        Send Message
+                    </button>
+                </form>
+
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontsize="large" />
+                    <GifIcon fontsize="large" />
+                    <EmojiEmotionsIcon fontsize="large" />
+                </div>
+            </div>
         </div>
     )
 }
