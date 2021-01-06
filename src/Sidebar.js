bb4bd5674106ef718da5a7b40548e4c8e26eb282
matheusferretti <matheusferretti96@gmail.com>
2021-01-06 15:47:28 -0500

    updating discord

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,6 +6,10 @@ import AddIcon from '@material-ui/icons/Add';
 import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import CallIcon from '@material-ui/icons/Call';
+import { Avatar } from '@material-ui/core';
+import MicIcon from '@material-ui/icons/Mic';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function Sidebar() {
     return (
@@ -46,6 +50,20 @@ function Sidebar() {
                 <div className="sidebar__voiceIcons">
                     <InfoOutlinedIcon />
                     <CallIcon />
+                </div>
+            </div>
+
+            <div className="sidebar__profile">
+                <Avatar />
+                <div className="sidebar__profileInfo">
+                    <h3>@matheusferrettii</h3>
+                    <p>#thisIsMyId</p>
+                </div>
+
+                <div className="sidebar__profileIcons">
+                    <MicIcon />
+                    <HeadsetIcon />
+                    <SettingsIcon />
                 </div>
             </div>
         </div>
