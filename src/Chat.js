fba41793e539e1aa44d97970a0f87680fd1b0242
matheusferretti <matheusferretti96@gmail.com>
2021-01-06 16:05:10 -0500

    updating discord

---
+++ src/Chat.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Chat.css'
+import ChatHeader from './ChatHeader'
+
+function Chat() {
+    return (
+        <div className="chat">
+            <h2>I am the chat screen!</h2>
+            <ChatHeader />
+        </div>
+    )
+}
+
+export default Chat
