3a8ae1f8ff9c13a974d3eb41d48f20ce723e8bdf
matheusferretti <matheusferretti96@gmail.com>
2021-01-06 16:30:44 -0500

    updating discord

--- src/ChatHeader.js
+++ src/ChatHeader.js
@@ -3,6 +3,9 @@ import "./ChatHeader.css";
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import EditLocationRoundedIcon from '@material-ui/icons/EditLocationRounded';
 import PeopleAltRoundedIcon from '@material-ui/icons/PeopleAltRounded';
+import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
+import SendRoundedIcon from '@material-ui/icons/SendRounded';
+import HelpRoundedIcon from '@material-ui/icons/HelpRounded';
 
 function ChatHeader() {
     return (
@@ -21,8 +24,12 @@ function ChatHeader() {
                 <PeopleAltRoundedIcon />
 
                 <div className="chatHeader__search">
-                    <input placeholderSearch />
+                    <input placeholder="Search" />
+                    <SearchRoundedIcon />
                 </div> 
+
+                <SendRoundedIcon />
+                <HelpRoundedIcon />
             </div>
         </div>
     )
