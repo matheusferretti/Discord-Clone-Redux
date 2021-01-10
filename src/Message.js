2004509751bb9a580826eba5347dfd9ad2a1a4c4
matheusferretti <matheusferretti96@gmail.com>
2021-01-10 00:18:26 -0500

    updating

--- src/Message.js
+++ src/Message.js
@@ -6,6 +6,12 @@ function Message() {
     return (
         <div className="message">
             <Avatar />
+            <div className="message__info">
+               <h4>matheusferrettii
+                   <span className="message__timestamp">this is a timestamp
+                   </span>
+                </h4> 
+            </div>
         </div>
     )
 }
