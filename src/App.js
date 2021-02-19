4b4a9b4391b703c97ea1b3db4ef6f3903d2f8888
matheusferretti <matheusferretti96@gmail.com>
2021-02-19 16:31:21 -0500

    new features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { userSelector } from 'react-redux';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 
 
 function App() {
+  const user = userSelector();
   return (
     <div className="app">
       <Sidebar />
