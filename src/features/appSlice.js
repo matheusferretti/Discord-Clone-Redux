24e9cf8388f37cce9f5b4f75a2b75dc67e790da3
matheusferretti <matheusferretti96@gmail.com>
2021-02-19 12:22:05 -0500

    features added

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -3,20 +3,19 @@ import { createSlice } from '@reduxjs/toolkit';
 export const appSlice = createSlice({
   name: 'app',
   initialState: {
-    app: null,
+    channelId: null,
+    channelName: null
   },
   reducers: {
-    login: (state, action) => {
+    setChannelId: (state, action) => {
       state.app += action.payload;
     },
-    logout: (state) => {
-      state.app = null;
-    }
   },
 });
 
-export const { login, logout } = appSlice.actions;
+export const { etChannelId } = appSlice.actions;
 
-export const selectCount = state => state.app.app;
+export const selectChannelId = (state) => state.app.channelId;
+export const selectChannelName = (state) => state.app.channelName;
 
 export default appSlice.reducer;
