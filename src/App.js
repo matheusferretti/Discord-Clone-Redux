5ec18f2801aa5cdc73042bcaaed67b61df38ff68
matheusferretti <matheusferretti96@gmail.com>
2021-02-22 16:18:28 -0500

    new features

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { userSelector } from 'react-redux';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import { selectUser } from './features/userSlice';
 
 
 function App() {
-  const user = userSelector();
+  const user = userSelector(selectUser);
   return (
     <div className="app">
-      <Sidebar />
-      <Chat />
+      {user? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <h2>You need to login!</h2>
+      )}
     </div>
   );
 }
